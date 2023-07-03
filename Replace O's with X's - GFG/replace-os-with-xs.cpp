a0a94c20e0c03c3edda7eac83e5048ddd77fb6ea
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2023-07-03 09:26:02 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    private:
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&mat){
+        // marking the ones we have now visited
+        vis[row][col]=1;
+        // check for top right bottom left
+        
+        int n=mat.size();       // these arays can be declared globally
+        int m=mat[0].size();
+        
+        int diffr[]={-1,0,+1,0};
+        int diffc[]={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nrow=row+diffr[i];
+            int ncol=col+diffc[i];
+            
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && mat[nrow][ncol]=='O'){
+                dfs(nrow,ncol,vis,mat);
+            }
+        }
+    }
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        //traverse the first row and last row
+        for(int j=0;j<m;j++){
+            
+            //first row
+            if(!vis[0][j] && mat[0][j]=='O'){
+                dfs(0,j,vis,mat);
+            }
+            
+            //last row
+            if(!vis[n-1][j] && mat[n-1][j]=='O'){
+                dfs(n-1,j,vis,mat);
+            }
+        }
+        
+        //traversing for first and last column
+        for(int i=0;i<n;i++){
+            //first column
+            if(!vis[i][0] && mat[i][0]=='O'){
+                dfs(i,0,vis,mat);
+            }
+            
+            //last column
+            if(!vis[i][m-1] && mat[i][m-1]=='O'){
+                dfs(i,m-1,vis,mat);
+            }
+        }
+        
+        //now traversing all the other boxes and checking for the ones that are present 
+        //inside the matrix and not no the border
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && mat[i][j]=='O'){
+                    mat[i][j]='X';
+                }
+            }
+        }
+        
+        return mat;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
