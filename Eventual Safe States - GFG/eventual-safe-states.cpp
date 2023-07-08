1f41624066905acc11a0cda44225d1e31b9436a6
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2023-07-08 21:28:46 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -9,56 +9,42 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    bool dfsCheck(int node,vector<int>adj[],int vis[],int vispath[],int check[]){
-        vis[node]=1;
-        vispath[node]=1;
-        //we have not yet confirmed if it is safe so check is zero for that node
-        check[node]=0;
-        
-        //traverse for adjacent nodes
-        for(auto it:adj[node]){
-            //when the node is not visited
-            if(vis[it]==0){
-                if(dfsCheck(it,adj,vis,vispath,check)==true){
-                    //if it has some other path then it canot be in check
-                    check[node]=0;
-                    return true;
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
+        //BFSAPPROACH USING TOPO SORT
+        //we reverse all the connections and perform a topological sort 
+        // this greatly simplifies the question
+        int inDegree[V]={0};
+        vector<int>adjRev[V];
+        for(int i=0;i<V;i++){
+            // it is a neighbor of i i.e    i->it
+            //and we are doing it->i
+            for(auto it:adj[i]){
+                adjRev[it].push_back(i);
+                inDegree[i]++;
+            }
+        }
+        queue<int>q;
+        vector<int>safeNodes;
+        for(int i=0;i<V;i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safeNodes.push_back(node);
+            for(auto it:adjRev[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                    q.push(it);
                 }
             }
-            //if the node has been previously visited
-            else if(vispath[it]==1){
-                check[node]=0;
-                return true;
-            }
         }
-        //if the node is not in a cycle and is in path then we can use it in check
-        check[node]=1;
-        vispath[node]=0; //emptying the array again for backtracking
-        return false;
-    }
-  public:
-    vector<int> eventualSafeNodes(int v, vector<int> adj[]) {
-        // code here
-        //A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node.
-        //visiting array
-        int vis[v]={0};
-        int vispath[v]={0};
-        //maintaining check to confirm that node to be in ans or not
-        int check[v]={0};
-        vector<int>safeNodes;
-        for(int i=0;i<v;i++){
-            if(vis[i]==0){
-                dfsCheck(i,adj,vis,vispath,check);
-            }
-        }
-        for(int i=0;i<v;i++){
-            if(check[i]==1){
-                safeNodes.push_back(i);
-            }
-        }
+        sort(safeNodes.begin(),safeNodes.end());
         return safeNodes;
-    
         
     }
 };
