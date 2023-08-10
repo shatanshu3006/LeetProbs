a040243f709244b1bdb10817dd4fbbfb3839263e
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2023-08-11 00:54:41 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,27 +5,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-
+  long long int func(int ind,int target,int coins[],vector<vector<long long int>>&dp){
+      if(ind==0){
+          return (target%coins[0]==0);
+      }
+      if(dp[ind][target]!=-1){
+          return dp[ind][target];
+      }
+      long long not_take=func(ind-1,target,coins,dp);
+      long long take=0;
+      if(coins[ind]<=target){
+          take=func(ind,target-coins[ind],coins,dp);
+          
+      }
+      return dp[ind][target]=take+not_take;
+  }
     long long int count(int coins[], int n, int sum) {
 
         // code here.
         //memooizations
-        // long long int sum = accumulate(coins, coins+n, 0LL);
-        vector<vector<long long int>>dp(n,vector<long long int>(sum+1,0));
-        for(int i=0; i<=sum; i++){
-            if(i%coins[0]==0) dp[0][i]=1;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0; j<=sum; j++){
-                long long not_take=dp[i-1][j];// func(ind-1,target,coins,dp);
-                long long take=0;
-                if(coins[i]<=j){
-                    take=dp[i][j-coins[i]];//func(ind,target-coins[ind],coins,dp);
-                }
-                dp[i][j]=take+not_take;
-            }
-        }
-        return dp[n-1][sum];
+        // long long int sum = accumulate
+        vector<vector<long long int>>dp(n,vector<long long int>(sum+1,-1));
+        // for()
+        // for(int i=0;i<n-1;i++){
+            
+        // }
+        return func(n-1, sum, coins, dp);
     }
 };
 
