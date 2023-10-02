40c6b6347b9af27365675e3d69cf80d94dc43429
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2023-10-03 01:43:43 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -109,14 +109,27 @@ class Solution{
         //  Your Code here
         return dfsHeight(root)!=-1;
     }
+    //us
     int dfsHeight(Node*root){
-        if(!root)return 0;
-        int leftheight=dfsHeight(root->left);
-        if(leftheight==-1)return -1;
-        int rightheight=dfsHeight(root->right);
-        if(rightheight==-1)return -1;
-        if(abs(rightheight-leftheight)>1)return -1;
-        return max(leftheight,rightheight)+1;
+        if(root==NULL){
+            return 0;
+        }
+        int leftHeight=dfsHeight(root->left);
+        if(leftHeight==-1){
+            return -1;
+        }
+        int rightHeight=dfsHeight(root->right);
+        if(rightHeight==-1){
+            return -1;
+        }
+        
+        if(abs(leftHeight-rightHeight)>1){
+            return -1;
+        }
+        else{
+            return max(leftHeight,rightHeight)+1;
+        }
+        
     }
 };
 
