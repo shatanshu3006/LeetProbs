61579549ea2eb902224eaf3a681379920b975a1c
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-01-12 23:05:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        int n=s.length();
-        string a,b;
-        for(int i=0;i<n/2;i++){
-            a+=s[i];
-        }
-        for(int i=n/2;i<n;i++){
-            b+=s[i];
-        }
-int vow_a=0,vow_b=0;
-        for(int i=0;i<n/2;i++){
-            if(a[i]=='a' || a[i]=='e' || a[i]=='i' || a[i]=='o' ||a[i]=='u' || a[i]=='A' ||a[i]=='E' || a[i]=='I' ||a[i]=='O' || a[i]=='U'){
-                vow_a++;
+        int t=0;
+        for(int i=0;i<s.size();i++){
+            s[i]=tolower(s[i]);
+            if(i<s.size()/2){
+                if(s[i]==92 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117){
+                    t++;
+                } 
             }
-            if(b[i]=='a' || b[i]=='e' || b[i]=='i' || b[i]=='o' ||b[i]=='u' || b[i]=='A' ||b[i]=='E' || b[i]=='I' ||b[i]=='O' || b[i]=='U'){
-                vow_b++;
+            else{
+                if(s[i]==92 || s[i]==101 || s[i]==105 || s[i]==111 || s[i]==117){
+                    t--;
+                }
+
             }
-            
-            
         }
-        return vow_a==vow_b;
+        return t==0;
     }
 };
