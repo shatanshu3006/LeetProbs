157d74803c2e48a8764e2188bc7d915d071b51fb
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-03-05 21:34:10 +0530

    Time: 27 ms (45.86%), Space: 14.1 MB (13.45%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            if(s[start]==s[end]){
+                while(s[start]==s[start+1] && start<end){
+                    start++;
+                }
+                while(s[end]==s[end-1] && start<end){
+                    end--;
+                }
+                //once the shrinking is done, move start and end by 1 unit 
+                //for next iteration
+                start++;
+                end--;
+            }
+            else{
+                break;
+            }
+
+        }
+        int ans=end-start+1;
+        if(ans<0)return 0;
+        return ans;
+    }
+};
