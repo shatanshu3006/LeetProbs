19d4b0360f6e15b1caa1af9b19acb16380832756
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-03-26 01:43:35 +0530

    Time: 0 ms (100%), Space: 7.3 MB (96.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,49 @@
 class Solution {
 public:
+//check for the first part and then later on using loops
+    //since the hash for i+1 need previous value of i but for the first window we do not have one so we handle this separately by check ing then later we use for loop from the index starting from the pattern length (len)
+typedef unsigned long long int ull;
     int strStr(string haystack, string needle) {
-        //solved using a find function , can be done using KMP algorithm too.
-        int ans= haystack.find(needle);
-        if(ans<0 || ans>haystack.length()){
-            return -1;
+        //using rabin karp
+        //TC: O(n+m)
+        //SC: O(1)
+
+        return rabinKarp(haystack,needle);
+    }
+
+    int rabinKarp(string &s,string &pat)
+    {   
+        // TC: O(m+n)
+        // SC: O(1)
+        
+        ull power = 1;
+        ull p = 131;
+        ull hash1 = 0, hash2 = 0;
+        
+        int len = pat.length();
+        for(int i = 1;i<len;i+=1)
+            power*=p;
+            
+        for(int i = 0;i<len;i+=1)
+        {
+            hash1 = hash1*p + pat[i];
+            hash2 = hash2*p + s[i];
         }
-        else{
-            return ans;
+        
+        if(hash1==hash2)
+            return 0;
+        
+        
+        for(int i = len;i<s.length();i+=1)
+        {
+            hash2 -= power*s[i-len];
+            hash2 = hash2*p + s[i];
+            
+            if(hash2==hash1)
+                return i-len+1;
+            
         }
+        
+        return -1;
     }
 };
