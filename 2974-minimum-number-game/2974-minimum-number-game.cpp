c00d3263cecd1df8e65efcce4e034e3e1dba16c0
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-03-28 14:46:54 +0530

    Time: 11 ms (28.84%), Space: 28.5 MB (9.24%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+//using priority queue
+//tc->o(nlogn)
+//sc->o(1)
     vector<int> numberGame(vector<int>& nums) {
-        if(nums.size()==0)return {};
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+        int i=0;
+        while(pq.size()>0){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
 
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i+=2){
-            swap(nums[i],nums[i+1]);
+            nums[i++]=b;
+            nums[i++]=a;
         }
         return nums;
     }
