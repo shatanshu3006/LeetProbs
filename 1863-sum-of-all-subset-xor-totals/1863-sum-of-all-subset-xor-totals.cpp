36ec7b94bab63bdcd789c7c4f3b764c3bbf09d54
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-03-30 16:45:48 +0530

    Time: 4 ms (55.74%), Space: 10.8 MB (19.49%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+vector<int>ans;
+vector<int>ds;
+
+void generateSubs(vector<int>&nums,int index){
+    if(index==nums.size()){
+        int xorAns=0;
+        for(auto it:ds){
+            xorAns^=it;
+        }
+        ans.push_back(xorAns);
+        return;
+    }
+    ds.push_back(nums[index]);
+    generateSubs(nums,index+1);
+    ds.pop_back();
+    generateSubs(nums,index+1);
+}
+    int subsetXORSum(vector<int>& nums) {
+        generateSubs(nums,0);
+        return accumulate(ans.begin(),ans.end(),0);
+    }
+};
