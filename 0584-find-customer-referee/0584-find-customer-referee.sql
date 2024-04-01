6d0225549b313914f644c2fe2ce441277354005f
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-04-01 22:56:37 +0530

    Time: 1057 ms (30.52%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
-# because , the null value does not have any values to be checked with the operator ( #= , !=) ,. So when we use referee_id = 2 , it will not check in the null places #beacuse there is no values. So we have to combine both the statement--
+#here COALESCE is used to replace NULL values with zero before checking whether it is #equal to 2 or not.
 
-select name from Customer where referee_id != 2 or referee_id is NULL
+select name from Customer where coalesce(referee_id,0)<>2;
