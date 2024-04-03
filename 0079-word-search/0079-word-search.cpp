cce8a675dfea7fdc6f3a0eff05c0dd178a64ce88
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-04-04 00:53:36 +0530

    Time: 325 ms (69.96%), Space: 8 MB (100%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int vis[7][7];
+    bool func(int i, int j, vector<vector<char>>&board , string word,int index){
+        int m = board.size();
+        int n = board[0].size();
+        if(index == word.size()){
+            return true;
+        }
+
+        int dr[4]={0,1,0,-1};
+        int dc[4]={1,0,-1,0};
+        for(int k=0;k<4;k++){
+            int nr = dr[k]+i;
+            int nc = dc[k]+j;
+            if(nr>=0 && nc>=0 && nr<m && nc<n){
+                if(board[nr][nc] == word[index]){
+                      if(!vis[nr][nc]){
+                      vis[nr][nc]=1;
+                      if(func(nr,nc,board,word,index+1)){
+                          return true;
+                      }
+                      vis[nr][nc]=0;
+                      }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        memset(vis,0,sizeof(vis));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]){
+                    vis[i][j]=1;
+                    if(func(i,j,board,word,1)){
+                        return true;
+                    }
+                    vis[i][j]=0;
+                }
+            }
+        }
+
+        return false;
+     
+
+    }
+};
