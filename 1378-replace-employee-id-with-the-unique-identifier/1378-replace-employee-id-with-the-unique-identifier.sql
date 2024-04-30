34f170f0c29b68d3e2a59033de5e428647b93ef9
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-04-30 20:52:24 +0530

    Time: 2149 ms (70.56%), Space: 0B (100%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select eu.unique_id as unique_id,e.name as name from Employees e left join EmployeeUNI eu on e.id=eu.id;
+
+#What is SQL LEFT JOIN?
+#SQL LEFT JOIN, also known as a LEFT OUTER JOIN, is a type of SQL JOIN operation that retrieves all records from the left table (table1) and the matching records from the right table (table2). If there are no matching records in the right table, NULL values are included for those columns.
+select eu.unique_id as unique_id,e.name as name from Employees as e left join EmployeeUNI as eu on e.id=eu.id;
