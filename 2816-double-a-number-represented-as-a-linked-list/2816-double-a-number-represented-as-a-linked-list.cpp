56924cccf1772be49e2069e721a25efbb3ba73da
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-05-08 03:02:41 +0530

    Time: 165 ms (81.43%), Space: 120.8 MB (72.46%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -10,28 +10,23 @@
  */
 class Solution {
 public:
-int twiceOfVal(ListNode* head){
-    if(!head){
-        return 0;
-    }
-
-    //double the val at the current node and add the res of next nodes 
-
-    int doubledValue=head->val*2 + twiceOfVal(head->next);
-
-    //update hte current node's value with the units digit of the result
-    head->val=doubledValue%10;
-
-    //return the carry (tens of the result)
-    return doubledValue/10;
-}
     ListNode* doubleIt(ListNode* head) {
-        int carry =twiceOfVal(head);
+        ListNode*p2=head;
+        ListNode*ans=new ListNode(0);
+        ListNode*p1=ans;
+        ans->next=head;
 
-        //if there is a carry, insert a new node at the beginning with the carry value
-        if(carry){
-            head=new ListNode(carry,head);
+        while(p2!=NULL){
+            int cal=(p2->val)*2;
+            p2->val=cal%10;
+            p1->val+=(cal/10);
+
+            p2=p2->next;
+            p1=p1->next;
         }
-        return head;
+        if(ans->val != 0)
+        return ans;
+        else 
+        return ans->next;
     }
 };
