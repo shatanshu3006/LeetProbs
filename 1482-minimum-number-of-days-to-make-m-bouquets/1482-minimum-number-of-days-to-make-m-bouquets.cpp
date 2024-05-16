136a0427b31edd022abf84e8cca14883284d50bc
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-05-17 00:58:17 +0530

    Time: 119 ms (34.28%), Space: 68.9 MB (91.26%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    bool good(vector<int>&bloomDay,int mid,int m,int k){
-        int cnt=0;
-        int n=0;
-        for(int i=0;i<bloomDay.size();i++){
-           if (bloomDay[i]<=mid){
-               n++;
-           }
-           else{
-               n=0;
-           }
-           if(n==k){
-               cnt++;
-               n=0;
-           }
-        }
-        if(cnt<m){
-            return false;
+bool good(vector<int>&bloomDay,int mid,int m,int k){
+    int cnt=0;
+    int n=0;
+    for(int i=0;i<bloomDay.size();i++){
+        if(bloomDay[i]<=mid){
+            n++;
         }
         else{
-            return true;
+            n=0;
+        }
+        if(n==k){
+            cnt++;
+            n=0;
         }
     }
-    // did a binary search on values from least to largest element of the vector
+    if(cnt<m){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-
-        if(n < (long long)m * k)
-            return -1;
-        int s=*min_element(bloomDay.begin(),bloomDay.end());
-        int e=*max_element(bloomDay.begin(),bloomDay.end());
-        int ans=e;
-        while(s<=e){
-            int mid=(s+e)/2;
-            if(good(bloomDay,mid,m,k)){
+        int low=*min_element(bloomDay.begin(),bloomDay.end());
+        int high=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=high;
+        int n=bloomDay.size();
+        if(n<(long long)m*k)return -1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(good(bloomDay,mid,m,k)==true){
                 ans=min(ans,mid);
-                e=mid-1;
+                high=mid-1;
             }
             else{
-                s=mid+1;
+                low=mid+1;
             }
         }
         return ans;
