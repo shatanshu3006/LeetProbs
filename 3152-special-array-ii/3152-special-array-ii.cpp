dde1d258240fecc4d8b8a03b00bfa2fa7c9c3829
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-05-19 12:21:55 +0530

    Time: 210 ms (40%), Space: 135.5 MB (40%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
-bool find(vector<int>&mp,int start,int end){
-    int n=mp.size();
-    int l=0,r=n-1;
-    while(l<=r){
-        int mid=(l+r)/2;
-        if(mp[mid]<start){
-            l=mid+1;
-        }
-        else{
-            if(mp[mid]>end){
-                r=mid-1;
-            }
-            else{
-                return true;
-            }
-        }
-    }
-    return false;
-}
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size();
-        //map to store all the indices of same parity
-        vector<int>mp;
-        for(int i=0;i<nums.size()-1;i++){
-            if((nums[i]%2==0 && nums[i+1]%2==1)||(nums[i]%2==1 && nums[i+1]%2==0)){
-                continue;
+        //prefi array
+        vector<int>store(nums.size(),0);
+        for(int i=1;i<nums.size();i++){
+            if((nums[i]%2)!=nums[i-1]%2){
+                store[i]=store[i-1]+1;
             }
             else{
-                mp.push_back(i);
+                store[i]=store[i-1];
             }
         }
-        vector<bool>ans;
+    vector<bool>ans;
         for(auto it:queries){
-            int start=it[0],end=it[1];
-            if(find(mp,start,end-1)){
-                ans.push_back(false);
+            int left=it[0],right=it[1];
+
+            if((store[right]-store[left])==(right-left)){
+                ans.push_back(true);
             }
             else{
-                ans.push_back(true);
+                ans.push_back(false);
             }
         }
         return ans;
