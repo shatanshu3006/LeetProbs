629e4189e1b224270f76aa9f0e392e789490310c
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-08 21:56:34 +0530

    Time: 172 ms (75%), Space: 142.7 MB (37.5%) - LeetHub

--- 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,28 +1,74 @@
 class Solution {
 public:
-    int findWinningPlayer(vector<int>& s, int k) {
-        int n=s.size();
-        deque<int>dq;
-        for(int i=1;i<n;i++){
-            dq.push_back(i);
-        }
-        int z=k,ele=0,w=0;
-        while(true){
-            int num=dq.front();
-            dq.pop_front();
-            if(s[num]<s[ele]){
-                w++;
-                dq.push_back(num);
+    int findWinningPlayer(vector<int>& skills, int k) {
+        int n=skills.size();
+        if(k==1){
+            if(skills[0]>skills[1]){
+                return 0;
             }
             else{
-                w=1;
-                dq.push_back(ele);
-                ele=num;
-            }
-            if(w==k || w>=n-1){
-                return ele;
+                return 1;
             }
         }
-        return 1;
+
+        deque<pair<int,int>>dq;
+        int maxi=INT_MIN,maxInd=-1,count=1;
+
+//pushingn into the queue and marking the highest val with maxInd
+        for(int i=0;i<n;i++){
+            dq.push_back({skills[i],i});
+
+            if(maxi<skills[i]){
+                maxi=skills[i];
+                maxInd=i;
+            }
+        }
+        if(n<k){
+            return maxInd;
+        }
+        maxi=INT_MIN,maxInd=-1;
+
+        int temp=n;
+        while(temp--){
+            auto firstVal=dq.front();dq.pop_front();
+            auto secondVal=dq.front();dq.pop_front();
+
+            if(firstVal.first>secondVal.first){
+                dq.push_front(firstVal);
+                dq.push_back(secondVal);
+
+                if(firstVal.first==maxi){
+                    count++;
+                    if(count==k){
+                        return firstVal.second;
+                    }
+
+                }
+                else{
+                    count=1;
+                }
+                maxi=firstVal.first;
+            }
+            else{
+                dq.push_front(secondVal);
+                dq.push_back(firstVal);
+
+                if(secondVal.first==maxi){
+                    count++;
+                    if(count==k){
+                        return secondVal.second;
+                    }
+                }
+                else{
+                    count=1;
+                }
+
+                maxi=secondVal.first;
+            }
+        }
+
+        return dq.front().second;
+
+
     }
 };
