6eabf5a70f0d822f9587785716be2bc3cb30cd37
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-09 13:35:10 +0530

    Time: 103 ms (34.46%), Space: 46.3 MB (41.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,15 +3,12 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         map<int,int>mp;
         mp[0]=1;
-        int j=0;
-        int ans=0;
-        int sum=0;
-        int n=nums.size();
+        int j=0;int ans=0;int sum=0;int n=nums.size();
         while(j<n){
             sum+=nums[j];
             int rem=sum-k;
 
-            //this means we havealready found this in the map
+            //already in the map 
             if(mp.find(rem)!=mp.end()){
                 ans+=mp[rem];
             }
