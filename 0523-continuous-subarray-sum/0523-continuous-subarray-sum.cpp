f4b11fe982a886407fb642ee2c74b8adc75fb33c
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-09 14:38:02 +0530

    Time: 239 ms (16.37%), Space: 135.1 MB (24.66%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int left=0,right=1,sum=0;
-        map<int,int>mp;
-        mp[0]=1;
-        while(right<n){
-            sum+=nums[right];
-            int rem=sum-k;
-            //if the rem exists in the array then till that index the sum exists
-            //if the sum exists then return true
-            //check que 560
-            if(mp.find(rem)!=mp.end()){
-                return true;
+        //instead of sums, this works on the remaninder sfound till that index when the sum id divided by k
+        map<int,int>remaindersFound;
+        int currSum=0;
+        remaindersFound[0]=-1;  // to handle the initla case when the subarray starts from the first index (we are keeping it -1)
+
+        for(int i=0;i<nums.size();i++){
+            currSum+=nums[i];
+            int rem=currSum%k;
+
+            if(remaindersFound.find(rem)!=remaindersFound.end()){
+                //check if the length is atleast 2
+                if(i-remaindersFound[rem]>=2){
+                    return true;
+                }
             }
             else{
-            mp[sum]++;
+                remaindersFound[rem]=i;
             }
-            right++;
         }
         return false;
     }
