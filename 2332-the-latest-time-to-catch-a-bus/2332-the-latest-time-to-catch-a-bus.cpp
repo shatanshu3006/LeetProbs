e1cd6267e852d619dd92382a8a3d31500f5ef91a
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-21 03:29:05 +0530

    Time: 187 ms (18.98%), Space: 81.3 MB (27.37%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int latestTimeCatchTheBus(vector<int>& b, vector<int>& p, int capacity) {
-        int n=b.size(),m=p.size(),j=0,ans=0,prev=0;
-
+        int n=b.size(),m=p.size();
+        int j=0,ans;
+        set<int>st;
         sort(b.begin(),b.end());
         sort(p.begin(),p.end());
 
         for(int i=0;i<n;i++){
-            int count=0;
-            while(j<m && p[j]<=b[i] && count<capacity){
-                if(p[j]-prev>1){
+            int c=0;
+            while(j<m && c<capacity && p[j]<=b[i]){
+                if(st.find(p[j]-1)==st.end()){  //not found in set
                     ans=p[j]-1;
+
                 }
-                prev=p[j];
+                st.insert(p[j]);
+                c++;
                 j++;
-                count++;
             }
-            if(count<capacity && b[i]-prev>=1){
+            //onelast possibility is if seat is still there and we dont have anyone arriving in the end of time of the current bus then we can be that person to come latest
+            if(c<capacity && st.find(b[i])==st.end()){ //capacity is there and there is no one to come on the last moment 
                 ans=b[i];
             }
         }
