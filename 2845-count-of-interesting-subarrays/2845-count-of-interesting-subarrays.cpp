2c7b02bd3714d0c16361fea2510244148b7e4241
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-23 02:02:05 +0530

    Time: 208 ms (19.79%), Space: 122.3 MB (36.69%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-#define ll long long
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        #define ll long long
         map<ll,ll>mp;
-        ll ans=0,prefix=0,n=nums.size();
         mp[0]=1;
-
-        //we will take prefix sum of nums[i]%modulo ==k
-        // [3,1,9,6] --(k==0,modulo==3)--> [1,0,1,1]
-        // prefix sum => [1,1,2,3] if its count==k till index i
-
-        for(int i=0;i<n;i++){
-            if(nums[i]%modulo==k){
+        int n=nums.size(); ll ans=0,prefix=0;
+        for(int it:nums){
+            if(it%modulo==k){
                 prefix++;
             }
-            prefix=prefix%modulo;
-            if(mp.find((prefix-k+modulo)%modulo)!=mp.end()){
-                ans+=mp[(prefix-k+modulo)%modulo];
-            }
-            mp[prefix]++;
+            ans+=mp[(prefix-k)%modulo];
+            mp[prefix%modulo]++;
         }
         return ans;
     }
