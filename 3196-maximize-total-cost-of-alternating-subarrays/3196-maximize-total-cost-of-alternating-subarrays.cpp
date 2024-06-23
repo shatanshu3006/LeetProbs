4af358a1393385ffb3ddf723f079728ce14258a2
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-06-23 12:13:31 +0530

    Time: 80 ms (57.14%), Space: 80.9 MB (42.86%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -1,47 +1,15 @@
 class Solution {
 public:
-typedef long long ll;
-ll func(vector<int>&nums,int index,bool flag,vector<vector<ll>>&dp){
-    int n=nums.size();
-    if(index>=n){
-        return 0;
-    }
-    if(dp[index][flag]!=-1){
-        return dp[index][flag];
-    }
-    ll take=LLONG_MIN,not_take=LLONG_MIN;
-    if(flag==false){    //negative sign (for subarray cost)
-        not_take=(-nums[index]+func(nums,index+1,true,dp));
-    }
-    //positive sign 
-    take=nums[index]+func(nums,index+1,false,dp);
-    return dp[index][flag]=max(take,not_take);
-}
     long long maximumTotalCost(vector<int>& nums) {
         int n=nums.size();
-        //two possibilities of flag->true and false (take and not_take)
-        vector<vector<ll>>dp(n+1,vector<ll>(3,-1));
-        //print(dp);
-        return nums[0]+func(nums,1,false,dp);
-        
+        vector<long long>dp(n+1,0);
+        dp[1]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            long long option1=dp[i]+nums[i];    //for both positive and negative nums[i]
+            long long option2=dp[i-1]+nums[i-1]+(-1*nums[i]);
+
+            dp[i+1]=max(option1,option2);
+        }
+        return dp[n];
     }
-    //for the first test case the table looks like:
-
-//initially ->
-//            true(0)  false(1)  (3)
-//        0      -1      -1      -1
-//        1      -1      -1      -1
-//        2      -1      -1      -1
-//        3      -1      -1      -1
-//        4      -1      -1      -1       
-
-//finally ->
-//            true(0)  false(1)  (3)
-//        0      -1       10     -1
-//        1       8       9      -1
-//        2       7       7      -1
-//        3      -4       4      -1
-//        4      -1      -1      -1  
-
-
 };
