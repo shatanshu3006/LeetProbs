383520ad864b0fa2ea9e476ebb531cee68f3b557
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-07-15 14:30:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int,int>mp;
-        vector<int>v;
-        for(auto it:nums){
-            mp[it]++;
-        }
-        for(auto it:mp){
-            if(it.second>floor(nums.size()/3)){
-                v.push_back(it.first);
+        //majority voting algo
+        int n=nums.size();
+        int cnt1=0,cnt2=0;
+        int el1=INT_MIN,el2=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && el2!=nums[i]){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1)cnt1++;
+            else if(nums[i]==el2)cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        return v;
+        vector<int>ans;
+        cnt1=0,cnt2=0; //reset the values
+        for(int i=0;i<n;i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+         int mini = int(n / 3) + 1;
+    if (cnt1 >= mini) ans.push_back(el1);
+    if (cnt2 >= mini) ans.push_back(el2);
+set<int>st;
+for(auto it:ans){
+    st.insert(it);
+}
+vector<int>v;
+    for(auto it:st){
+        v.push_back(it);
+    }
+    return v;
+
+
 
     }
 };
