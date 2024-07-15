224f6fe3ef0b2c99018eb3b776cfa1d6e73512ed
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-07-15 14:40:07 +0530

    Time: 3 ms (33.88%), Space: 6.5 MB (100%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,10 +10,9 @@ int func(int i,int j,vector<vector<int>>&dp){
     if(dp[i][j]!=-1){
         return dp[i][j];
     }
-    int up=0,left=0;
-    up = func(i-1,j,dp);
-    left=func(i,j-1,dp);
-    return dp[i][j]=left+up;
+    int up = func(i-1,j,dp);
+    int left=func(i,j-1,dp);
+    return dp[i][j]=up+left;
 }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
