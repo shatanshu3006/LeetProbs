28ffd090e3ff11cd65963a3479c82c8041a16f27
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-07-17 02:06:24 +0530

    Time: 3 ms (98.96%), Space: 10.1 MB (71.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int>map(128,0);
-        for(char c:t){
-            map[c]++;
+        int n=s.length();
+        vector<int>freq(128,0);
+        int cnt=0;
+        //calculating the total number of unique chars in t
+        for(auto it:t){
+            freq[it-'A']++;
+            if(freq[it-'A']==1)cnt++;
         }
-        int counter=t.size();
-        int begin=0;int end=0;int d=INT_MAX;int head=0;
-        while(end<s.size()){
-            if(map[s[end++]]-- > 0){
-                counter--;
-            }
-            while(counter==0){
-                if(end-begin<d){
-                    head=begin;
-                    d=end-head;
+        int ans=INT_MAX,ind=-1;
+        int i=0,j=0;
+        while(j<n){
+            freq[s[j]-'A']--;
+            if(freq[s[j]-'A']==0)cnt--;
+            while(i<=j && cnt==0){
+                freq[s[i]-'A']++;
+                if(freq[s[i]-'A']>0)cnt++;
+                if(ans>(j-i+1)){
+                    ans=j-i+1;
+                    ind=i;
                 }
-                if(map[s[begin++]]++ ==0){
-                    counter++;
-                }
+                i++;
             }
+            j++;
         }
-        return d == INT_MAX ? "" : s.substr(head, d);
+        if(ind==-1)return "";
+        return s.substr(ind,ans);
+
     }
 };
