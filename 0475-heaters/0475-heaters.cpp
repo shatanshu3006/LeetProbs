e7947cbeb3f03a32425b70d67e1257b60e744b2b
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-07-26 12:23:25 +0530

    Time: 155 ms (8.1%), Space: 29.2 MB (28.18%) - LeetHub

---
+++ 0475-heaters/0475-heaters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+bool check (vector<int>& houses, vector<int>& heaters,int mid){
+    bool found=true;
+    for(int i=0;i<houses.size();i++){
+        auto it1=lower_bound(heaters.begin(),heaters.end(),houses[i]-mid);
+        if(it1==heaters.end()){
+            return false;
+        }
+        if(*it1>houses[i]+mid){
+            return false;
+        }
+    }
+    return true;
+}
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+     sort(houses.begin(),houses.end());
+     sort(heaters.begin(),heaters.end());
+     int low=0,high=INT_MAX;
+     int ans=-1;
+     while(low<=high){
+        int mid=low+(high-low)/2;
+        if(check(houses,heaters,mid)==true){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+     }   
+     return ans;
+    }
+};
