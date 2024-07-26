58330f0689fe02d98d1e2ca2a5c14ad5b07ae543
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-07-26 16:05:30 +0530

    Time: 7 ms (93.59%), Space: 10.1 MB (33.18%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int numSub(string s) {
+
+        //setting countgaps to zero wil lead to not counting of 1 length subarrys containing 1 , sp we either set totalSum to zero and countGaps to 1 or we set totalSum to total number of 1's for 1 length 1 congtaining subarrays and countGaps to 0
         const int mod=1e9+7;
         //this is for single length subarrays
-        //long long totalSum=count(s.begin(),s.end(),'1');
-        long long totalSum=0;
+        long long totalSum=count(s.begin(),s.end(),'1');
+        //long long totalSum=0;
         int l=0,n=s.size();
         while(l<n){
             if(s[l]=='0'){
@@ -12,7 +14,7 @@ public:
             }
             else{
                 int r=l+1;
-                long long countGaps=1;
+                long long countGaps=0;
                 while(s[l]==s[r] && r<n){
                     countGaps++;
                     r++;
