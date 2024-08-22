a7644fc178b177693d0dac176254803af44d032b
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-08-22 11:07:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        int fibnm1=fib(n-1);
-        int fibnm2=fib(n-2);
-        return fibnm1 +fibnm2;
+        int[] dp;
+        dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
