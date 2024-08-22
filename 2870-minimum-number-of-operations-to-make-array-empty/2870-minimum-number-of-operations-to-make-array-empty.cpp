c0a4dad174e3555acec0f42b38d8542b94ed749b
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-08-22 15:43:38 +0530

    Time: 132 ms (23.11%), Space: 97.1 MB (7.09%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto a: nums){
-            mp[a]++;
-        }
-
-        int count=0;
-        for(auto a: mp){
-            int t = a.second;
-            if(t<2)return -1;
-            if(t%3==0){
-                count+=t/3;
-            }
-            else if(t%3==2){
-                count+=(t-2)/3+1;
-            }
-            else if(t%3==1){
-                count+=(t-1)/3+1;
+        map<int,int>mp;
+        for(auto it:nums)mp[it]++;
+        vector<int>arr;
+        int ans=0;
+        for(auto it:mp)arr.push_back(it.second);
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=1)return -1;
+            else if(arr[i]%3==0)ans+=arr[i]/3;
+            else if(arr[i]%3==1 || arr[i]%3==2){
+                ans+=(arr[i]/3 +1);
             }
         }
-        return count;
+        return ans;
     }
 };
