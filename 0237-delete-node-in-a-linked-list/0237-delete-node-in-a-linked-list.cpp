8e9a213e6a57775408a674fadefa584a1b005c73
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-09-11 18:58:00 +0530

    Time: 7 ms (63.07%), Space: 12.7 MB (85.56%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -8,8 +8,9 @@
  */
 class Solution {
 public:
+//This method will not work if we need to delete the last node of the linked list since there is no immediate successor. However, the problem description explicitly states that the node to be deleted is not the tail node in the list.
     void deleteNode(ListNode* node) {
-        node->val=node->next->val;
-        node->next=node->next->next;
+         node->val=node->next->val;
+         node->next=node->next->next;
     }
 };
