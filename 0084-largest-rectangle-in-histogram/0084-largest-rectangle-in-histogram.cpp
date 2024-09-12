247b50924d5953855345900c6cf04b79f51f5390
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-09-12 09:05:10 +0530

    Time: 100 ms (83.56%), Space: 80 MB (74.89%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        int max_area=0;
+    int largestRectangleArea(vector<int>& histo) {
         stack<int>st;
-        //declaring the leftsmall array for all the elements and rightsmall array
-        int leftSmall[n];
-        int rightSmall[n];
-
-        //making the leftSmall array
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
+        int maxA=0;
+        int n=histo.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || histo[st.top()]>histo[i])){
+                int height=histo[st.top()];
                 st.pop();
-            }
-
-            if(st.empty()){
-                leftSmall[i]=0;
-            }
-            else{
-                leftSmall[i]=st.top()+1;
+                int width;
+                if(st.empty()){
+                    width=i;
+                }
+                else{
+                    width=i-st.top()-1;
+                }
+                maxA=max(maxA,height*width);
             }
             st.push(i);
         }
-
-        //clearing the stack to be reused
-        while(!st.empty()){
-            st.pop();
-        }
-
-        //making the rightSmall array
-        //traversing in the reverse but doing the same operations
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                rightSmall[i]=n-1;
-            }
-            else{
-                rightSmall[i]=st.top()-1;
-            }
-            st.push(i);
-        }
-        //iterating again for the max area
-        for(int i=0;i<n;i++){
-            max_area=max(max_area,heights[i]*(rightSmall[i]-leftSmall[i]+1));
-        }
-        return max_area;
-
+        return maxA;
     }
 };
