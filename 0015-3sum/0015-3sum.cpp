74b54575a8e654c8cc28795a9f7ad2735d90a1a6
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-09-19 09:50:22 +0530

    Time: 71 ms (38.79%), Space: 29.3 MB (49.74%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
-        
         int n=nums.size();
-        set<vector<int>>bigset;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            set<int>st;
-            for(int j=i+1;j<n;j++){
-                int third=-(nums[i]+nums[j]);
-
-                if(st.find(third)!=st.end()){
-                    vector<int>temp={nums[i],nums[j],third};
-                    sort(temp.begin(),temp.end());
-                    bigset.insert(temp);
+            if(i!=0 && nums[i]==nums[i-1])continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
                 }
-                st.insert(nums[j]);
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    vector<int>temp={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+
+                    //skipping the duplicates
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                }
             }
-            
-        }
-        for(auto it:bigset){
-            ans.push_back(it);
+
         }
         return ans;
     }
