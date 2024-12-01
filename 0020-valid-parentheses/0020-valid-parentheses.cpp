efb4412688f49257384a1660861f8aa5fd21d8e6
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-12-02 00:09:55 +0530

    Time: 4 ms (7.15%), Space: 7.6 MB (98.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char> st;
-      for(auto i:s){
-        if(i=='(' || i=='{' || i=='[') 
-          st.push(i);
-        else{
-          if(!st.empty()){
-            char temp=st.top();
-            if( (i==')'&&temp=='(') || (i==']'&&temp=='[') || (i=='}'&&temp=='{') )
-              st.pop();
-            else return false;
-          }
-          else return false;
+        stack<char>st;
+        for(auto it:s){
+            if(it=='(' || it=='{' || it=='['){
+                st.push(it);
+            }
+            else{
+                if(!st.empty()){
+                    char temp=st.top();
+                    if((it==')' && temp=='(') || (it==']' && temp=='[') || (it=='}' && temp=='{')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
         }
-      }
-      if(!st.empty())
-          return false;
-        else
-          return true;
+        if(!st.empty()){
+            return false;
+        }
+        return true;
     }
 };
