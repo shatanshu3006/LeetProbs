dc4a093aacbf231d1c6e8a99241e784d145b7eff
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2024-12-02 12:35:34 +0530

    Time: 31 ms (9.17%), Space: 8.9 MB (100%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        // create a dp vector for storing the longest subset till the index i which satisfies the divisibility condition  initially dp vector is filled with 1 (since every element itself is a subset with length 1 )
+		int ans=0;
+        vector<int>store(n,0);
+        //vector store is used to store the index of the previously included elements
+        int lastId=0;
+        // 1) sort the given array  so that we need to check only in for A[i] % A[j] not A[j] % A[i] provided  i > j
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n;i++){
+            store[i]=i;
+
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
+                    dp[i]=1+dp[j];
+                    store[i]=j;
+                }
+            }
+            if(ans<dp[i]){
+                ans=dp[i];
+                lastId=i;           //stores the index where we got the max length of the subset
+            }
+        }
+
+        vector<int>printAns;
+        printAns.push_back(nums[lastId]);
+
+        while(store[lastId]!=lastId){
+            lastId=store[lastId];
+            printAns.push_back(nums[lastId]);
+        }
+        //since we have the ans in reverse we just reverese the vector we got
+        reverse(printAns.begin(),printAns.end());
+
+        return printAns;
+		 
+    }
+};
