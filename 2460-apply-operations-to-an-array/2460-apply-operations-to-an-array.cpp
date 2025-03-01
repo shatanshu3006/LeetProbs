c8de8da6bb88f60a7a909d88ecf9852a0c8b3cf3
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2025-03-01 18:37:43 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]+=nums[i];
+                nums[i+1]=0;
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                ans.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
