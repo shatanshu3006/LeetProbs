631bcdce0fee50ccbddcdbb9736112b13a22a8ec
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2025-08-03 19:10:18 +0530

    Time: 0 ms (100%), Space: 17.1 MB (33.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,60 +1,70 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+    int orangesRotting(vector<vector<int>>& mat) {
+        // Code here
+        int n=mat.size();
+        int m=mat[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-
-        int ans=0;
-
         queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0}); //initial time when rotting begins
-                    vis[i][j]=1;      //visited this point as it has rotten
-                }
-                else if(grid[i][j]==0){
-                    vis[i][j]=1;      //visited the position and it is empty
-                }
-                else{
-                    vis[i][j]=0;      //we have not yet come here
+                if(mat[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
                 }
             }
         }
-
-        int diffRow[]={0,0,1,-1};
-        int diffCol[]={1,-1,0,0};
-
+        int time=0;
+        
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-
-            ans=max(ans,t);
+            auto node=q.front();
             q.pop();
-
-            for(int i=0;i<4;i++){
-                int neighRow=r+diffRow[i];
-                int neighCol=c+diffCol[i];
-
-                if(neighRow>=0 && neighRow<n && neighCol>=0 && neighCol<m && vis[neighRow][neighCol]==0 && grid[neighRow][neighCol]==1){
-                    q.push({{neighRow,neighCol},t+1});
-                    //increment the time ans and push new coordinates
-                    vis[neighRow][neighCol]=1;
-                }
+            int curr_time=node.second;
+            time=max(time,curr_time);
+            int x=node.first.first;
+            int y=node.first.second;
+            //up
+            if(x>=1){
+            if(mat[x-1][y]==1 && vis[x-1][y]==0){
+                vis[x-1][y]=2;
+                q.push({{x-1,y},curr_time+1});
+                //time=max(time,curr_time+1);
             }
+            }
+            //left
+            if(y>=1){
+            if(mat[x][y-1]==1 && vis[x][y-1]==0){
+                vis[x][y-1]=2;
+                q.push({{x,y-1},curr_time+1});
+                //time=max(time,curr_time+1);
+            }
+            }
+            
+            //right
+            if(y<=m-2){
+            if(mat[x][y+1]==1 && vis[x][y+1]==0){
+                vis[x][y+1]=2;
+                q.push({{x,y+1},curr_time+1});
+                //time=max(time,curr_time+1);
+            }
+            }
+            //down
+            if(x<=n-2){
+            if(mat[x+1][y]==1 && vis[x+1][y]==0){
+                vis[x+1][y]=2;
+                q.push({{x+1,y},curr_time+1});
+               // time=max(time,curr_time+1);
+            }
+            }
+            
         }
-
-        //checking if all the points have been covered using the visited aray
-        //if not then that has not rotten since we can reach there through traversal but if it is zero then it is not rotten at all 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0){
+                if(mat[i][j]==1 && vis[i][j]==0){
                     return -1;
                 }
             }
         }
-        return ans;
+        return time;
     }
 };
