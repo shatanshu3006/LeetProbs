8a3b34cdb37a0854b1ac928261a2f70463ab54d0
Shatanshu Bodkhe <100084737+shatanshu3006@users.noreply.github.com>
2025-08-21 19:50:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-
-int solve(int ind,vector<int>&dp,int n){
-    if(ind>n)return 0;
-    if(ind==n)return 1;
-
-    if(dp[ind]!=-1)return dp[ind];
-
-    int take=solve(ind+1,dp,n);
-    int not_take=solve(ind+2,dp,n);
-    return dp[ind]=take+not_take;
-}
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return solve(0,dp,n);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
